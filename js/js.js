dc8b14b60bacc3b27091a66bdbcebe9b9c9df426
6ukhwa <qwe1212485@gmail.com>
2025-03-19 04:26:30 +0900

    250319

--- js/js.js
+++ js/js.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
     // $('#loader').removeClass('loaded');
     // $('#loader').addClass('loaded');
     
-
+    // top 버튼
+    $(".topBtn").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+    });
 
 }); //돈땃쥐
